420276e053e6138ba284ee62b632c43da68dba37
Yusuke Yamamoto <motsulab@gmail.com>
2018-10-14 11:55:08 +0900

    getIRData member function added

--- src/ir_receiver.cpp
+++ src/ir_receiver.cpp
@@ -1,3 +1,4 @@
+#include <arduino.h>
 #include "ir_receiver.h"
 
 IRReceiver::IRReceiver(byte pin_receiver, bool invert_out)
@@ -16,6 +17,9 @@ IRReceiver::IRReceiver(byte pin_receiver, bool invert_out)
 
     // set led pin mode
     pinMode(_pin, INPUT);
+
+    _status = IRStatus::stop;
+
 }
 
 byte IRReceiver::input()
@@ -47,4 +51,150 @@ void IRReceiver::getIR(IRWave &wave, long samples)
         wave.append(value);
         while(micros() < tlim) delayMicroseconds(1);
     }
+}
+
+int IRReceiver::_waitSignal(long timeout_ms)
+{
+    unsigned long t0 = millis();
+    while(input() == 0)
+        if(millis() - t0 > timeout_ms && timeout_ms > 0)
+            return -1;
+
+    return millis() - t0;
+}
+
+int IRReceiver::_scanLeaderCode(IRData &data, long high_time, long low_time, bool format_autodetect)
+{
+    if(high_time >= 2000 && high_time < 5000)
+    {
+        if(low_time >= 1000 && low_time < 2500)
+        {
+            // AEHA Leader Code detected.
+            _status = IRStatus::leader_detected;
+            if (format_autodetect)
+            {
+                data.format = IRDataFormat::AEHA;
+            }
+        }
+        else if(low_time >= 2500 && low_time < 5000)
+        {
+            // AEHA Repeat Code detected.
+            _status = IRStatus::repeat_detected;
+            data.is_repeat_code = true;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else if(high_time >= 8000 && high_time < 12000)
+    {
+        if (low_time >= 4000 && low_time < 6000)
+        {
+            // NEC Leader Code detected.
+            _status = IRStatus::leader_detected;
+            if (format_autodetect)
+            {
+                data.format = IRDataFormat::NEC;
+            }
+        }
+        else if(low_time >= 2000 && low_time < 3000)
+        {
+            // NEC Repeat Code detected.
+            _status = IRStatus::repeat_detected;
+            data.is_repeat_code = true;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int IRReceiver::_getDataBit(long high_time, long low_time)
+{
+    // Hardcoding: current available format:AEHA & NEC
+    if(high_time >= 350 && high_time < 800)
+    {
+        if(low_time >= 350 && low_time < 800)
+        {
+            // '0' received
+            return 0;
+        }
+        else if(low_time >= 1000 && low_time < 2000)
+        {
+            // '1' received
+            return 1;
+        }
+    }
+    // invalid data code error
+    return -1;
+}
+
+int IRReceiver::getIRData(IRData &data, long timeout_ms)
+{
+    byte signal;
+    long high_time = 0,low_time = 0;
+    unsigned long t0, t1;
+    bool leader_detected = false;
+    bool stop_detected = false;
+    int databit;
+
+    data.clear();
+
+    // wait for signal
+    if(_waitSignal(timeout_ms) == -1)
+    {
+        // timeout return
+        return -1;
+    }
+    _status = IRStatus::signal_detected;
+
+    t0 = micros();
+    while(1)
+    {
+        signal = input();
+        if(signal == 0)
+        {
+            if(low_time > 8000)
+            {
+                // Stop bit detect
+                _status = IRStatus::stop;
+                break;
+            }
+            t1 = micros();
+            low_time += t1 - t0;
+            t0 = t1;
+        }
+        else
+        {
+            if(low_time > 0)
+            {
+                switch (_status)
+                {
+                    case IRStatus::signal_detected:
+                        if(_scanLeaderCode(data, high_time, low_time) == -1)
+                        {
+                            // invalid leader code
+                            return -1;
+                        }
+                        break;
+                    case IRStatus::leader_detected:
+                        databit = _getDataBit(high_time, low_time);
+                        if(databit >= 0) data.append(databit);
+                        break;
+                    case IRStatus::repeat_detected:
+                        break;
+                }
+                high_time = 0;
+                low_time = 0;
+            }
+            t1 = micros();
+            high_time += t1 - t0;
+            t0 = t1;
+        }
+    }
+
+    return 0;
 }
--- src/ir_receiver.h
+++ src/ir_receiver.h
@@ -2,17 +2,31 @@
 #define _IR_RECEIVER_H_
 
 #include "ir_wave.h"
+#include "ir_data.h"
 
 class IRReceiver
 {
+public:
+    enum IRStatus
+    {
+        stop = 0,
+        signal_detected,
+        leader_detected,
+        repeat_detected
+    };
 private:
     byte _pin;
     //unsigned int _sampling_period;
     byte _input_high, _input_low;
+    IRStatus _status;
+    int _waitSignal(long timeout_ms = -1);
+    int _scanLeaderCode(IRData &data, long high_time, long low_time, bool format_autodetect = true);
+    int _getDataBit(long high_time, long low_time);
 public:
     IRReceiver(byte pin_receiver, bool invert_out = false);
     byte input();
     void getIR(IRWave &wave, long samples = -1);
+    int getIRData(IRData &data, long timeout_ms = -1);
 };
 
 #endif //_IR_RECEIVER_H_
